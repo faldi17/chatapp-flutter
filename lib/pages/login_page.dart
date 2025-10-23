3c17b3e57e29c56c6308752dc885f7281021b085
Faldi <faldiansyahk@gmail.com>
2025-10-24 03:28:37 +0700

    light mode theme

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+    );
+  }
+}
