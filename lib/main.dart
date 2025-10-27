1e76b5359de368b96574a4fde28e521bc207d229
Faldi <faldiansyahk@gmail.com>
2025-10-27 08:12:39 +0700

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/firebase_options.dart';
 import 'auth/login_or_register.dart';
 import '../themes/light.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
