9029428287cac08785d63a63af11f9d71c88d76f
Faldi <faldiansyahk@gmail.com>
2025-11-18 14:29:30 +0700

    send receive message

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/services/auth/auth_service.dart';
+import 'package:flutter_basic/services/chat/chat_service.dart';
 
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
+  final String receiverID;
 
-  const ChatPage({super.key, required this.receiverEmail});
+  ChatPage({super.key, required this.receiverEmail, required this.receiverID});
+
+  // text controller
+  final TextEditingController _messageController = TextEditingController();
+
+  // chat & auth services
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
+
+  // send message
+  void sendMessage() async {
+    // if there is something inside the textfield
+    if (_messageController.text.isNotEmpty) {
+      // send the message
+      await _chatService.sendMessage(receiverID, _messageController.text);
+
+      // clear text controller
+      _messageController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(receiverEmail)));
+    return Scaffold(
+      appBar: AppBar(title: Text(receiverEmail)),
+      body: Column(
+        children: [
+          // display all messages
+          Expanded(child: _buildMessageList()),
+
+          // user input
+        ],
+      ),
+    );
+  }
+
+  // build message list
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+      stream: _chatService.getMessage(receiverID, senderID),
+      builder: (context, snapshot) {
+        // error
+
+        // loading
+
+        // return list view
+      },
+    );
   }
 }
